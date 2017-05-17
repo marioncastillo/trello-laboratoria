bb7d97ab2ae5049978360aae670d49354e915a83
MarionFoto <marionacp@gmail.com>
2017-05-17 13:52:52 -0400

    iniciando ejercicio

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function nuevaLista(){
+
+    var añadirLista = document.getElementById("añadir-lista");
+    añadirLista.addEventListener("click", function(){
+        alert("Está funcionando");
+    })
+};
+
+nuevaLista();
